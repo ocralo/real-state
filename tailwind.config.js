c7c249ab613f91644368feb7e055bd7222b0ce30
ocralo <ocralo@gmail.com>
2021-09-30 15:34:47 -0500

    feat: add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: '#3ECFAF',
+      dark: '#012D26',
+      ligth: '#E5F0EE',
+      white: '#FFF',
+    },
+    fontFamily: {
+      silka: ['Silka', 'sans-serif'],
+    },
+  },
+}
