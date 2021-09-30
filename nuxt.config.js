c9ecd1ab4e7f83bdb311c6b743919cf75b958a55
ocralo <ocralo@gmail.com>
2021-09-30 16:16:21 -0500

    feat: correction title an metadata page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,19 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'real-state',
+    title: 'La Haus más que una inmobiliaria ¡Compra finca raíz a tu ritmo!',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'es-CO',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '¿Buscas una inmobiliaria? Tenemos una buena noticia, somos mucho más. La tecnología y oferta de finca raíz harán más fácil la compra de tu vivienda nueva.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
