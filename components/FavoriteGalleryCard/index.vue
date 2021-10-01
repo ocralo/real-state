776853c00f4fce6956b8688de8d495d0c2b64369
ocralo <ocralo@gmail.com>
2021-10-01 01:14:51 -0500

    feat: create FavoriteGalleryCard -create validator props -create structure -add class styles -create function computed

---
+++ components/FavoriteGalleryCard/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-72">
+    <GalleryCard />
+    <p class="text-lg font-semibold mt-3">Mis favoritos</p>
+    <p class="text-sm text-secundary mt-1">
+      {{ totalImages }} propiedades guardadas
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FavoriteGalleryCard',
+  props: {
+    title: {
+      type: String,
+      default() {
+        return 'Mis favoritos'
+      },
+    },
+    images: {
+      type: Array,
+      default() {
+        return []
+      },
+      validator(value) {
+        const objKeys = Object.keys(value)
+        return (
+          (objKeys.includes('images') && objKeys.includes('id')) ||
+          value.length === 0
+        )
+      },
+    },
+  },
+  computed: {
+    totalImages() {
+      const images = this.images
+      return images.length || 0
+    },
+  },
+}
+</script>
