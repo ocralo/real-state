76b0b6f58558053bed46a60acf47614023929f2b
ocralo <ocralo@gmail.com>
2021-09-30 21:09:30 -0500

    feat: add text

--- lang/en/index.js
+++ lang/en/index.js
@@ -1,5 +1,9 @@
 module.exports = {
   welcome: 'welcome',
+  'arrow down': 'arrow down',
+  Cundinamarca: 'Cundinamarca',
+  Antioquia: 'Antioquia',
+  Recursos: 'Recursos',
   'My Profile': 'My Profile',
   menu: 'menu',
   'brand la haus': 'logo la haus',
--- lang/es/index.js
+++ lang/es/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   welcome: 'bienvenido',
+  'arrow down': 'flecha abajo',
   'My Profile': 'Mi Perfil',
+  Cundinamarca: 'Cundinamarca',
+  Antioquia: 'Antioquia',
+  Recursos: 'Recursos',
   menu: 'menu',
   'brand la haus': 'brand la haus',
 }
