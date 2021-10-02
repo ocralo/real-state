cda96eab711b415412e694dd85a595a91ec47b8d
ocralo <ocralo@gmail.com>
2021-10-02 00:40:35 -0500

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       dark: '#012D26',
       ligth: '#E5F0EE',
       white: '#FFF',
+      'md-blue': '#EBEDFF',
+      'sky-blue': '#3948FF',
     },
     fontFamily: {
       silka: ['Silka', 'sans-serif'],
